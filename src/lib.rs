bcac2aa8705e3219648cec2b9662242998cc4626
dewkul <jirapat0811@gmail.com>
2022-12-07 00:42:25 +0700

    lint: format syntax

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,30 @@
+pub mod data;
 /// # forexpros-wss
-/// 
+///
 /// Simple secured websocket (wss) client to forexpros.com stream--a realtime source for investing.com.
-/// 
+///
 /// # Example
-/// 
+///
 /// ```
 /// use forexpros_wss::push;
-/// 
+///
 /// fn main ( ) {
 /// 	let pair_id = "945629";	// BTC/USD
 /// 	//let pair_id = "8984";	// HK50 future
-/// 	
+///
 /// 	let handler = |s| {
 /// 		println ! ( "INFO: {:?}", s );
-///		
+///
 ///         // stop the stream
 ///		    Err (())
 /// 	};
-/// 
+///
 /// 	let stream = push::Stream::new ( pair_id.to_string ( ), handler )
 /// 		.expect ( "Failed to create stream" );
-/// 	
+///
 /// 	println ! ("main: stream.pair_id={}", stream.pair_id);
 /// 	println ! ("main: stream.handler={:?}", stream.stream_handle_spawn);
-/// 	
+///
 /// 	tokio::runtime::Runtime::new ( )
 /// 		.unwrap ( )
 /// 		.block_on (
@@ -31,11 +32,11 @@
 /// 		).unwrap ( ).unwrap ( );
 /// }
 /// ```
-/// 
+///
 /// 20:00 THA 03/12/2020
-/// 
+///
 /// # References
-/// 
+///
 ///	https://docs.rs/tokio-tungstenite/0.12.0/tokio_tungstenite/
 ///	https://docs.rs/tungstenite/0.11.1/tungstenite/
 ///	https://docs.rs/tokio/0.3.5/tokio/
@@ -44,6 +45,4 @@
 ///	https://github.com/websockets-rs/rust-websocket/issues/160
 ///	https://stackoverflow.com/questions/26946646/rust-package-with-both-a-library-and-a-binary/26946705#26946705
 ///	https://www.reddit.com/r/rust/comments/k5sb9o/tokio_block_onjoinhandle_freeze_randomly/
-
 pub mod push;
-pub mod data;
